3ae2435a33a6b572fc5682e9c919c160dfff0584
katayama800 <tattu.0310@gmail.com>
2024-10-07 00:24:38 +0900

    Add @katayama8000/cosense-client

--- src/average_wake_up_time.ts
+++ src/average_wake_up_time.ts
@@ -1,17 +1,11 @@
 import { dayjs } from "./libs/dayJs";
 import { formatDate } from "./libs/formatDate";
-import type { PageResponse } from "./types/PageResponse";
+import { CosenseClient } from "@katayama8000/cosense-client/mod";
 
 const fetchWakeUpTime = async (pageDate: string): Promise<string> => {
-    const pageUrl = `https://scrapbox.io/api/pages/katayama8000/${encodeURIComponent(pageDate)}`;
+    const client = CosenseClient("katayama8000");
+    const data = await client.getPage(pageDate);
 
-    const res = await fetch(pageUrl);
-    if (!res.ok) {
-        console.error(`No page found for ${pageDate}`);
-        return "Error fetching wake-up time";
-    }
-
-    const data: PageResponse = await res.json();
     const index = data.lines.findIndex(line => line.text.includes("起床時間"));
 
     if (index === -1 || !data.lines[index + 1]) {
