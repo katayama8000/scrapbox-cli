b885a6fcf0eb9f74566e5b156ef01558d8050413
katayama8000 <tattu.0310@gmail.com>
2026-01-15 23:20:36 +0900

    Revert "refactor: remove wake and sleep features from daily and weekly blog templates" (#54)
    This reverts commit 4453064e8d1e041dfa2b10103d4aad598cf7b6a0.

--- src/application/use-cases/blog.ts
+++ src/application/use-cases/blog.ts
@@ -4,17 +4,21 @@ import { ScrapboxPage } from "@/domain/models/scrapbox-page.ts";
 import { formatDate } from "@/infrastructure/adapters/formatters/formatDate.ts";
 import { formatTextItems } from "@/infrastructure/adapters/formatters/formatTextItems.ts";
 import { DateProviderImpl } from "@/infrastructure/adapters/date/date-provider-impl.ts";
+import { CalculateAverageWakeUpTimeUseCase } from "@/application/use-cases/average_wake_up_time.ts";
+import { CalculateAverageSleepQualityUseCase } from "@/application/use-cases/calculate_average_sleep_quality.ts";
 import { ScrapboxPayloadBuilder } from "@/infrastructure/adapters/scrapbox/scrapbox-payload-builder.ts";
 
 // Daily Blog
 const dailyTemplate = {
   buildText: (connectLink: string): string => {
     return formatTextItems([
+      { content: "Wake-up Time", format: "paragraph1" },
       { content: "Today's Tasks", format: "paragraph1" },
       {
         content: "https://tatsufumi.backlog.com/board/FAMILY",
         format: "nestedPlain",
       },
+      { content: "Score sleep quality", format: "paragraph1" },
       { content: "How was the day?", format: "paragraph1" },
       { content: connectLink, format: "link" },
       { content: "daily", format: "link" },
@@ -68,8 +72,16 @@ export class PostDailyBlogUseCase {
 
 // Weekly Blog
 const weeklyTemplate = {
-  buildText: (connectLink: string): string => {
+  buildText: (
+    connectLink: string,
+    avgWakeUpTime: number,
+    avgSleepQuality: number,
+  ): string => {
     return formatTextItems([
+      { content: "Last week's average wake-up time", format: "strong" },
+      { content: ` ${avgWakeUpTime.toString()}h`, format: "plain" },
+      { content: "Last week's average sleep quality", format: "strong" },
+      { content: ` ${avgSleepQuality.toString()}`, format: "plain" },
       { content: "Goals", format: "strong" },
       { content: "Try somwthing new", format: "strong" },
       { content: "How was the week", format: "strong" },
@@ -95,13 +107,26 @@ export class PostWeeklyBlogUseCase {
   constructor(
     private readonly scrapboxRepository: ScrapboxRepository,
     private readonly dateProvider: DateProvider,
+    private readonly calculateAverageWakeUpTimeUseCase:
+      CalculateAverageWakeUpTimeUseCase,
+    private readonly calculateAverageSleepQualityUseCase:
+      CalculateAverageSleepQualityUseCase,
   ) {}
 
   async execute(projectName: string): Promise<void> {
     const today = this.dateProvider.now();
     const title = weeklyTemplate.generateTitle(today);
     const connectLinkText = this.getConnectLinkText(today);
-    const content = weeklyTemplate.buildText(connectLinkText);
+    const avgWakeUpTime = await this.calculateAverageWakeUpTimeUseCase.execute(
+      projectName,
+    );
+    const avgSleepQuality = await this.calculateAverageSleepQualityUseCase
+      .execute(projectName);
+    const content = weeklyTemplate.buildText(
+      connectLinkText,
+      avgWakeUpTime,
+      avgSleepQuality,
+    );
 
     const page = ScrapboxPage.create({ projectName, title, content });
 
